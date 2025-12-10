3d0c0dd000a3d5e5d97e5c2cb81f242f7d0f40ad
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2025-12-10 15:55:45 +0530

    Added: program to check attendance

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation program");
+
+
+        int IS_FULL_TIME=1;
+
+        int empCheck=(int)(Math.floor(Math.random()*10)%2);
+        if(empCheck==IS_FULL_TIME)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
