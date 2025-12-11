b76f0c175105433e044e386b07e029dc635724b2
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2025-12-11 11:47:01 +0530

    Added: logic to manage full time and part time employees

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,28 @@ public class Main {
 
 
         int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
         int WAGE_PER_HOUR = 20;
         int FULL_DAY_HOURS = 8;
+        int PART_TIME_HOURS = 4;
 
-        int empCheck=(int)(Math.floor(Math.random()*10)%2);
-        if(empCheck==IS_FULL_TIME) {
+        int empCheck = (int)(Math.floor(Math.random()*10)%3);
+
+        int curHours = 0;
+
+        if(empCheck == IS_FULL_TIME) {
             System.out.println("Employee is Present");
-            int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
-            System.out.println("Daily Employee Wage: " + dailyWage);
+            curHours = FULL_DAY_HOURS;
+        }
+        else if(empCheck == IS_PART_TIME)
+        {
+            System.out.println("Employee is part time");
+            curHours = PART_TIME_HOURS;
         }
         else
             System.out.println("Employee is Absent");
+
+        int dailyWage = curHours * WAGE_PER_HOUR;
+        System.out.println("current day wage = " + dailyWage);
     }
 }
